1e6430e54f1d1983f2fbe0e1bb7548d92a148c8b
erdemnagikho <erdemnayir@gmail.com>
2026-09-01 14:19:19 +0300

    feat: initialize NestJS project with MongoDB, Drizzle ORM, and domain-driven design base structures

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongoModule } from './shared/infrastructure/database/mongodb/mongo.module';
+import { DrizzleModule } from './shared/infrastructure/database/postgres/drizzle.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongoModule,
+    DrizzleModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
